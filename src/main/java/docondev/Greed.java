834f10a46fb1c0368124b0638e564974ce9326c5
Doc Norton <doc@docondev.com>
2021-05-12 21:22:38 -0400

    Refactored to handle individual sets

--- src/main/java/docondev/Greed.java
+++ src/main/java/docondev/Greed.java
@@ -7,40 +7,38 @@ import java.util.stream.Collectors;
 public class Greed {
     private List<Integer> dice = new ArrayList<>();
 
+    public void addDice(Integer die) {
+        if (die < 1 || die > 6 ) throw new IllegalArgumentException("Die value must be between 1 and 6");
+        this.dice.add(die);
+    }
+
     public Integer score() {
         Integer result = 0;
-        List<List<Integer>> sets = createValueSets();
-        for ( List<Integer> set : sets ) {
+        for ( List<Integer> set : createValueSets()) {
             result += scoreSet(set);
         }
         return result;
     }
 
-    private List<List<Integer>> createValueSets() {
-        return dice.stream()
-                .collect(Collectors.groupingBy(die -> die))
-                .values().stream()
-                .collect(Collectors.toList());
-    }
-
     private Integer scoreSet(List<Integer> set) {
+        if (set.size() == 1) return scoreSingles(set);
+        if (set.size() == 2) return scoreDoubles(set);
         if (set.size() == 3) return scoreTriples(set);
         if (set.size() == 4) return scoreQuads(set);
         if (set.size() == 5) return scoreQuints(set);
         if (set.size() == 6) return scoreSects(set);
-        return set.stream().map( die -> scoreOneDie(die)).reduce(0, Integer::sum);
+        return 0;
     }
 
-    private Integer scoreSects(List<Integer> dice) {
-        return 8 * scoreTriples(dice);
+    private Integer scoreSingles(List<Integer> dice) {
+        Integer die = dice.get(0);
+        if ( die == 1) return 100;
+        if ( die == 5) return 50;
+        return 0;
     }
 
-    private Integer scoreQuints(List<Integer> dice) {
-        return 4 * scoreTriples(dice);
-    }
-
-    private Integer scoreQuads(List<Integer> dice) {
-        return 2 * scoreTriples(dice);
+    private Integer scoreDoubles(List<Integer> dice) {
+        return 2 * scoreSingles(dice);
     }
 
     private Integer scoreTriples(List<Integer> dice) {
@@ -49,14 +47,22 @@ public class Greed {
         return dieValue * 100;
     }
 
-    private Integer scoreOneDie(Integer die) {
-        if ( die == 1) return 100;
-        if ( die == 5) return 50;
-        return 0;
+    private Integer scoreQuads(List<Integer> dice) {
+        return 2 * scoreTriples(dice);
     }
 
-    public void addDice(Integer die) {
-        if (die < 1 || die > 6 ) throw new IllegalArgumentException("Die value must be between 1 and 6");
-        this.dice.add(die);
+    private Integer scoreQuints(List<Integer> dice) {
+        return 4 * scoreTriples(dice);
+    }
+
+    private Integer scoreSects(List<Integer> dice) {
+        return 8 * scoreTriples(dice);
+    }
+
+    private List<List<Integer>> createValueSets() {
+        return dice.stream()
+                .collect(Collectors.groupingBy(die -> die))
+                .values().stream()
+                .collect(Collectors.toList());
     }
 }
