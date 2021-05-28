3dfdbcf5b358f430ef24992efc81ce4a2ac0a839
Doc Norton <doc@docondev.com>
2021-05-28 11:18:48 -0500

    Change test signatures

--- src/test/java/docondev/GreedTest.java
+++ src/test/java/docondev/GreedTest.java
@@ -1,5 +1,6 @@
 package docondev;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -8,15 +9,20 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GreedTest {
 
+    private Greed greed;
+
+    @Before
+    public void setUp() throws Exception {
+        greed = new Greed();
+    }
+
     @Test
-    public void score_one_onehundred() {
-        Greed greed = new Greed();
+    public void score_1_Return_100() {
         assertThat(greed.score(new Integer[] {1}), is(equalTo(100)));
     }
 
     @Test
-    public void score_five_fifty() {
-        Greed greed = new Greed();
+    public void score_5_Return_50() {
         assertThat(greed.score(new Integer[] {5}), is(equalTo(50)));
     }
 }
