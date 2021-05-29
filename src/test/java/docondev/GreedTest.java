fdb8c647bfaca380009cb5920f42f333bc6e3a15
Doc Norton <doc@docondev.com>
2021-05-29 09:24:38 -0500

    Correct greed constructor back to accepting an array

--- src/test/java/docondev/GreedTest.java
+++ src/test/java/docondev/GreedTest.java
@@ -20,42 +20,42 @@ public class GreedTest {
 
     @Test
     public void score_Given_1_Return_100() {
-        assertThat(greed.score(new Rolls(makeSingle(1))), is(equalTo(100)));
+        assertThat(greed.score(makeSingle(1)), is(equalTo(100)));
     }
 
     @Test
     public void score_Given_5_Return_50() {
-        assertThat(greed.score(new Rolls(makeSingle(5))), is(equalTo(50)));
+        assertThat(greed.score(makeSingle(5)), is(equalTo(50)));
     }
 
     @Test
     public void score_Given_NonScoringSingle_Return_0() {
-        assertThat(greed.score(new Rolls(makeSingle(3))), is(equalTo(0)));
+        assertThat(greed.score(makeSingle(3)), is(equalTo(0)));
     }
 
     @Test(expected = InvalidParameterException.class)
     public void score_Given_GreaterThan6_Throw_ParameterError() {
-        assertThat(greed.score(new Rolls(makeSingle(7))), is(equalTo(0)));
+        assertThat(greed.score(makeSingle(7)), is(equalTo(0)));
     }
 
     @Test(expected = InvalidParameterException.class)
     public void score_Given_LessThan1_Throw_ParameterError() {
-        assertThat(greed.score(new Rolls(makeSingle(0))), is(equalTo(0)));
+        assertThat(greed.score(makeSingle(0)), is(equalTo(0)));
     }
 
     @Test
     public void score_Given_TripleOne_Return_1000() {
-        assertThat(greed.score(new Rolls(makeTriple(1))), is(equalTo(1000)));
+        assertThat(greed.score(makeTriple(1)), is(equalTo(1000)));
     }
 
     @Test
     public void score_Given_TripleTwo_Return_200() {
-        assertThat(greed.score(new Rolls(makeTriple(2))), is(equalTo(200)));
+        assertThat(greed.score(makeTriple(2)), is(equalTo(200)));
     }
 
     @Test
     public void score_Given_TripleThree_Return_300() {
-        assertThat(greed.score(new Rolls(makeTriple(3))), is(equalTo(300)));
+        assertThat(greed.score(makeTriple(3)), is(equalTo(300)));
     }
 
     private Integer[] makeSingle(int value) {
