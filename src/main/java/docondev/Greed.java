67e5298e528ce425b54e6d54e1c1e665fb1c589d
Doc Norton <doc@docondev.com>
2021-05-29 09:21:45 -0500

    Extract scoreSingle

--- src/main/java/docondev/Greed.java
+++ src/main/java/docondev/Greed.java
@@ -4,11 +4,14 @@ import java.security.InvalidParameterException;
 
 public class Greed {
     public Integer score(Rolls rolls) {
-        Integer firstDigit = rolls.getFirstDigit();
-        if (firstDigit <1 || firstDigit >6) throw new InvalidParameterException();
+        if (rolls.getFirstDigit() <1 || rolls.getFirstDigit() >6) throw new InvalidParameterException();
         if (rolls.isaTriple()) return rolls.scoreTriple();
-        if (firstDigit ==1) return 100;
-        if (firstDigit ==5) return 50;
+        return scoreSingle(rolls);
+    }
+
+    private int scoreSingle(Rolls rolls) {
+        if (rolls.getFirstDigit() ==1) return 100;
+        if (rolls.getFirstDigit() ==5) return 50;
         return 0;
     }
 
