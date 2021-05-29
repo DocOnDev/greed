b1b9c5d267466237d69f931de1361afdc7f4daf3
Doc Norton <doc@docondev.com>
2021-05-29 15:20:06 -0500

    Inline methods to prepare for refactoring

--- src/main/java/docondev/Rolls.java
+++ src/main/java/docondev/Rolls.java
@@ -13,40 +13,28 @@ public class Rolls {
     }
 
     int score() {
-        if (isaTriple()) return scoreTriple();
-        if (isaQuad()) return scoreQuad();
-        if (isaQuint()) return scoreQuint();
-        return scoreSingle();
-    }
-
-    private boolean isaTriple() {
-        return value.length == 3;
-    }
-
-    private int scoreTriple() {
-        if (baseDigit ==1) return 1000;
-        return baseDigit * 100;
-    }
-
-    private boolean isaQuad() {
-        return value.length == 4;
-    }
-
-    private int scoreQuad() {
-        return scoreTriple() * 2;
-    }
-
-    private boolean isaQuint() {
-        return value.length == 5;
-    }
-
-    private int scoreQuint() {
-        return scoreTriple() * 4;
-    }
-
-    private int scoreSingle() {
-        if (baseDigit ==1) return 100;
-        if (baseDigit ==5) return 50;
+        if (value.length == 1) {
+            if (baseDigit == 1) return 100;
+            if (baseDigit == 5) return 50;
+        }
+        if (value.length == 3) {
+            if (baseDigit ==1) return 1000;
+            return baseDigit * 100;
+        }
+        if (value.length == 4) {
+            int result = 1000;
+            if (baseDigit != 1) {
+                result = baseDigit * 100;
+            }
+            return result * 2;
+        }
+        if (value.length == 5) {
+            int result = 1000;
+            if (baseDigit != 1) {
+                result = baseDigit * 100;
+            }
+            return result * 4;
+        }
         return 0;
     }
 
