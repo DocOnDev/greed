0b145a7f9a339739e3f5503ec03ecc156ecf3d10
Doc Norton <doc@docondev.com>
2021-05-29 09:17:12 -0500

    Change scoreTriple to accept rolls

--- src/main/java/docondev/Greed.java
+++ src/main/java/docondev/Greed.java
@@ -6,14 +6,14 @@ public class Greed {
     public Integer score(Rolls rolls) {
         Integer firstDigit = rolls.getFirstDigit();
         if (firstDigit <1 || firstDigit >6) throw new InvalidParameterException();
-        if (rolls.isaTriple()) return scoreTriple(rolls.getValue());
+        if (rolls.isaTriple()) return scoreTriple(rolls);
         if (firstDigit ==1) return 100;
         if (firstDigit ==5) return 50;
         return 0;
     }
 
-    private int scoreTriple(Integer[] value) {
-        if (value[0]==1) return 1000;
-        return value[0] * 100;
+    private int scoreTriple(Rolls value) {
+        if (value.getFirstDigit()==1) return 1000;
+        return value.getFirstDigit() * 100;
     }
 }
